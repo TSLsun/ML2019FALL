560ab0cb3c39e60dbfb9a89f51be08ce5b6421e9
TSLsun <smartsunnytsai@gmail.com>
2019-12-13 21:57:52 +0800

    update hw5_train.sh

--- hw5/hw5_train.sh
+++ hw5/hw5_train.sh
@@ -3,6 +3,6 @@ python3 train_w2v.py "./"
 python3 train_rnn.py "./" "w2v_model.bin"
 
 
-#/share/home/tslsun025/.conda/envs/ml2019fall_hw3/bin/python3.6 make_token.py $1 $2 $3 
-#/share/home/tslsun025/.conda/envs/ml2019fall_hw3/bin/python3.6 make_token.py train_w2v.py "./" 
-#/share/home/tslsun025/.conda/envs/ml2019fall_hw3/bin/python3.6 make_token.py train_rnn.py "./" "w2v_model.bin"
+#/share/home/tslsun025/.conda/envs/ml2019fall_hw5/bin/python3.6 make_token.py $1 $2 $3 
+#/share/home/tslsun025/.conda/envs/ml2019fall_hw5/bin/python3.6 train_w2v.py "./" 
+#/share/home/tslsun025/.conda/envs/ml2019fall_hw5/bin/python3.6 train_rnn.py "./" "w2v_model.bin"
